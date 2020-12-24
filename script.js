e8ffc51cc1b281b946c3e319e6abe2fd86cc5533
Unknown <onlyformywork2033@gmail.com>
2020-12-24 18:09:30 +1000

    Home work lesson 1

---
+++ script.js
@@ -0,0 +1,32 @@
+// Create a Celsius temperature
+let temperatureCelsius = 36.6;
+// Do convertation to Pharyngate temperature
+let temperaturePharyngate = (9 / 5) * temperatureCelsius + 32;
+// Display result
+alert(temperaturePharyngate);
+
+
+// Create a name variable
+let name = 'Василий';
+// Create a admin variable with value of name
+let admin = name;
+// Display admin
+alert(admin)
+
+
+// Fourh task try
+let stingWithNumber = 1000 + '108' /
+    alert(stingWithNumber)
+
+/*
+Я так понял знак / это перенос
+Понятно что если убрать / то переменной будет данно значение NaN
+*/
+
+/*
+defer и async
+
+Оба атрибута позволяют продолжать загружать дом дерево старницы, и по путно загружать скрипты JS.
+Разница в последовательности загрузки defer отображать все скрипты последовательно(так как они идут в HTML),
+а async будет отображать скрипты сразу по окончанию загрузки.
+*/
